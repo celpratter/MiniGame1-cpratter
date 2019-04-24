fb874a22749d9374246abd24ca0f105bfeefa688
celpratter <celina.pratter@htl-salzburg.ac.at>
2019-04-24 15:14:01 +0200

    added script, change scenes

---
+++ Assets/MyGame/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadSceneMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void LoadSceneEnd()
+    {
+        SceneManager.LoadScene("End");
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
