6146310670ef5fa25bc913082094948aa71659d1
celpratter <celina.pratter@htl-salzburg.ac.at>
2019-05-07 19:53:32 +0200

    added player and pickup sprite, added Player script, SpawnPickups script and collider

---
+++ Assets/MyGame/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rigidbody2d;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rigidbody2d.velocity = new Vector2(speed * -1, 0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rigidbody2d.velocity = new Vector2(speed * 1, 0f);
+        }
+
+    }
+}
---
+++ Assets/MyGame/Scripts/SpawnPickups.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPickups : MonoBehaviour
+{
+
+    public GameObject flower;
+    // public GameObject seeds;
+    // public GameObject cheese;
+    // public GameObject egg;
+
+    public float spawnTime = 2f;
+    private float timer = 0;
+    private int randomNumber;
+    public float fallSpeed;
+
+    // Use this for initialization
+    public void SpawnRandom()
+    {
+        Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width),
+         Random.Range(900, Screen.height), Camera.main.farClipPlane / 2));
+
+        Instantiate(flower, screenPosition, Quaternion.identity);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > spawnTime)
+        {
+            SpawnRandom();
+            timer = 0;
+        }
+    }
+}
