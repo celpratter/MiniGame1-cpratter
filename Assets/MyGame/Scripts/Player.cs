3fd7aefb44585d834ff610657b45449d729a18f5
celpratter <celina.pratter@htl-salzburg.ac.at>
2019-05-26 20:46:46 +0200

    added ScoreCount Script

--- Assets/MyGame/Scripts/Player.cs
+++ Assets/MyGame/Scripts/Player.cs
@@ -8,26 +8,27 @@ public class Player : MonoBehaviour
 
     private const string AXISHORIZONTAL = "Horizontal";
     private float moveSpeed = 6.0f;
-    private int score = 0;
-    public TextMeshProUGUI pointsScore;
+    // private int score = 0;
+    // public TextMeshProUGUI pointsScore;
     private void Move()
     {
+        transform.position.z == 0;
         var deltaX = Input.GetAxis(AXISHORIZONTAL) * Time.deltaTime * moveSpeed;
         var newPosX = Mathf.Clamp(transform.position.x + deltaX,
             -300f, 300f);
 
         transform.position = new Vector2(newPosX, transform.position.y);
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("TriggerEvent " + collision.gameObject.name);
-        if (collision.gameObject.name == "Flower")
-        {
-            score += 5;
-            Debug.Log("score " + score);
-            pointsScore.text = score.ToString();
-        }
-    }
+    // private void OnTriggerEnter2D(Collider2D collision)
+    // {
+    //     Debug.Log("TriggerEvent " + collision.gameObject.name);
+    //     if (collision.gameObject.name == "Flower")
+    //     {
+    //         score += 5;
+    //         Debug.Log("score " + score);
+    //         pointsScore.text = score.ToString();
+    //     }
+    // }
     void Update()
     {
         Move();
