6e0f2f8268d0ba080935b39fac67f58c6ebb6612
celpratter <celina.pratter@htl-salzburg.ac.at>
2019-05-27 19:54:25 +0200

    changed Timer Script, added TimeOver screen and retry button

--- Assets/MyGame/Scripts/Timer.cs
+++ Assets/MyGame/Scripts/Timer.cs
@@ -7,20 +7,29 @@ public class Timer : MonoBehaviour
 {
     public int timeLeft = 60;
     public TextMeshProUGUI countdown;
+    public Canvas timeOver;
+    public SpriteRenderer darkCanvas;
 
     void Start()
     {
         StartCoroutine("LoseTime");
         Time.timeScale = 1;
+        timeOver.enabled = false;
+        darkCanvas.enabled = false;
     }
     void Update()
     {
         countdown.text = (" " + timeLeft);
+        if (timeLeft == 0)
+        {
+            timeOver.enabled = true;
+            darkCanvas.enabled = true;
+        }
     }
 
     IEnumerator LoseTime()
     {
-        while (true)
+        while (timeLeft > 0)
         {
             yield return new WaitForSeconds(1);
             timeLeft--;
