78d5e067a6851a6e116c0def7962578b22594bc2
celpratter <celina.pratter@htl-salzburg.ac.at>
2019-06-04 10:15:32 +0200

    added score to end screen

---
+++ Assets/MyGame/Scripts/ShowScore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowScore : MonoBehaviour
+{
+    public TextMeshProUGUI scorePoints;
+    public MyScore scoreCount;
+
+    void Start()
+    {
+        scorePoints.text = scoreCount.score.ToString();
+    }
+}
