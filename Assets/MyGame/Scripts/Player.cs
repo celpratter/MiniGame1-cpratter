248d661420456ee1e21780bac6a5c0ff2cb7a2e7
celpratter <celina.pratter@htl-salzburg.ac.at>
2019-06-05 15:08:56 +0200

    change color of sprite on trigger

--- Assets/MyGame/Scripts/Player.cs
+++ Assets/MyGame/Scripts/Player.cs
@@ -10,9 +10,11 @@ public class Player : MonoBehaviour
     private float moveSpeed = 6.0f;
     public TextMeshProUGUI pointsScore;
     public MyScore scoreCount;
+    SpriteRenderer spriteColor;
     void Start()
     {
         scoreCount.score = 0;
+        spriteColor = GetComponent<SpriteRenderer>();
     }
     private void Move()
     {
@@ -22,7 +24,7 @@ public class Player : MonoBehaviour
 
         transform.position = new Vector2(newPosX, transform.position.y);
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private IEnumerator OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("TriggerEvent " + collision.gameObject.name);
         if (collision.gameObject.name == "Flower(Clone)")
@@ -42,6 +44,19 @@ public class Player : MonoBehaviour
             scoreCount.score -= 10;
             Debug.Log("score " + scoreCount.score);
             pointsScore.text = scoreCount.score.ToString();
+            spriteColor.color = new Color(1, 0, 0, 1);
+            yield return new WaitForSeconds(0.1f);
+            spriteColor.color = new Color(1, 1, 1, 1);
+            yield return new WaitForSeconds(0.1f);
+            spriteColor.color = new Color(1, 0, 0, 1);
+            yield return new WaitForSeconds(0.1f);
+            spriteColor.color = new Color(1, 1, 1, 1);
+            yield return new WaitForSeconds(0.1f);
+            spriteColor.color = new Color(1, 0, 0, 1);
+            yield return new WaitForSeconds(0.1f);
+            spriteColor.color = new Color(1, 1, 1, 1);
+            yield return new WaitForSeconds(0.1f);
+
         }
     }
     void Update()
